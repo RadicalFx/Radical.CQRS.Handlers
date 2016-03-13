84c6d888da71e47c9c216e220ea0d15bdb1fd6cd
Mauro Servienti <mauro@topics.it>
2016-03-13 08:59:13 +0100

    support for getting latest version

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Radical.CQRS
 
+[![Build status](https://ci.appveyor.com/api/projects/status/mxqxvt54qewxe8sh?svg=true)](https://ci.appveyor.com/project/radical-bot/radical-cqrs-handlers)
+
 We (@nazarenomanco and I) have a dream: allow everyone to use CQRS to implement better architectures.
 
 Radical.CQRS is our way to help people in the above direction.
--- src/Radical.CQRS.Handlers/AbstractAggregateCommandHandler.cs
+++ src/Radical.CQRS.Handlers/AbstractAggregateCommandHandler.cs
@@ -1,25 +1,72 @@
 ﻿using Jason.Handlers.Commands;
 using Radical.CQRS.Messages;
+using System;
 
 namespace Radical.CQRS.Handlers
 {
     public abstract class AbstractAggregateCommandHandler<TAggregate, TCommand>
         : AbstractCommandHandler<TCommand>
-        where TAggregate : IAggregate
-        where TCommand : class, IAggregateCommand
+        where TAggregate : class, IAggregate
+        where TCommand : class
     {
         public IRepositoryFactory RepositoryFactory { get; set; }
 
         protected override object OnExecute(TCommand command)
         {
+            TAggregate aggregate = null;
+
             using(var session = this.RepositoryFactory.OpenSession())
             {
-                var aggregate = session.GetById<TAggregate>(command.Id, command.Version);
+                var aggregateId = this.GetAggregateId(command);
+                var aggregateVersion = this.GetAggregateVersion(command);
+                if(aggregateVersion.HasValue)
+                {
+                    aggregate = session.GetById<TAggregate>(AggregateQuery.GetLatest(aggregateId, aggregateVersion.Value));
+                }
+                else
+                {
+                    aggregate = session.GetById<TAggregate>(aggregateId);
+                }
+
                 this.Manipulate(aggregate, command);
                 session.CommitChanges();
 
                 return aggregate.Id;
             }
+        }
+
+        protected virtual int? GetAggregateVersion(TCommand command)
+        {
+            var iac = command as IAggregateCommand;
+            if(iac != null)
+            {
+                return iac.Version;
+            }
+
+            var pi = command.GetType().GetProperty("Version");
+            if(pi != null)
+            {
+                return (int)pi.GetValue(command);
+            }
+
+            return null;
+        }
+
+        protected virtual Guid GetAggregateId(TCommand command)
+        {
+            var iac = command as IAggregateCommand;
+            if(iac != null)
+            {
+                return iac.Id;
+            }
+
+            var pi = command.GetType().GetProperty("Id");
+            if(pi != null)
+            {
+                return (Guid)pi.GetValue(command);
+            }
+
+            return Guid.Empty;
         }
 
         public abstract void Manipulate(TAggregate aggregate, TCommand command);
